8d1f2cd1d9e8e7f4171f904f4f3bc96eb4223925
AnastasiaKozhemyachko <anastasiakozhemyachko@gmail.com>
2019-02-25 02:52:35 +0300

    Task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 
+var str1='';
+var str2='';
+var str_s='';
+
+    for (var i = 0; i < str.length; i++) {     
+        for (var j = 0; j < bracketsConfig.length; j++) {  
+            if (str.charAt(i)==bracketsConfig[j][0] && str.charAt(i+1)==bracketsConfig[j][1]) {             
+                    str1=str.substr(0,i);
+                    str2=str.substr(i+2,str.length);
+                str=str1+str2;
+                i=i-2;
+                }
+            }
+        }
+    if(str==''){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
